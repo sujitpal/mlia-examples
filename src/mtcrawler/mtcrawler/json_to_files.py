642c78abe8904c5f676c7261d16826a219744340
Sujit Pal <sujit.pal@comcast.net>
2014-08-19 13:26:18 -0700

    Updating to merge duplicate data (categories and keywords) and produce only 2.2k unique docs instead of 5k docs.

--- src/mtcrawler/mtcrawler/json_to_files.py
+++ src/mtcrawler/mtcrawler/json_to_files.py
@@ -6,7 +6,7 @@ import os
 import urllib
 
 DATA_DIR = "/home/sujit/Projects/mlia-examples/data/mtcrawler"
-JSON_FILE = "crawl_results.json"
+JSON_FILE = "mtsamples.json"
 
 p = re.compile(r"\d+-(.*)")
 
@@ -24,7 +24,8 @@ def get_md5(s):
     m = md5.new()
     m.update(s)
     return m.hexdigest()
-    
+
+md5_url_map = dict()
 fin = open(os.path.join(DATA_DIR, JSON_FILE), 'rb')
 jobj = json.load(fin)
 fin.close()
@@ -37,6 +38,9 @@ for rec in jobj[0]:
     sample_name = remove_leading_number(normalize(rec["sample_name"]))
     body = rec["body"].encode("utf-8")
     md5_body = get_md5(body)
+    if not md5_url_map.has_key(md5_body):
+        # record md5 to link (first link only) for multiple docs
+        md5_url_map[md5_body] = rec["link"]
     print "%s/%s.html" % (type_name, sample_name)
     unique_recs.add(md5_body)
     dir_name = os.path.join(rawhtml_dir, type_name)
@@ -48,4 +52,11 @@ for rec in jobj[0]:
     fout = open(os.path.join(dir_name, sample_name) + ".html", 'wb')
     fout.write(body)
     fout.close()
+# write out md5 to URL value, will be used to put the link back into the final
+# JSON files
+furls = open(os.path.join(DATA_DIR, "urls.csv"), 'wb')
+for key, value in md5_url_map.items():
+    furls.write("%s\t%s\n" % (key, value))
+furls.close()
+
 print "# unique records:", len(unique_recs)
--- src/mtcrawler/mtcrawler/parse_html_to_json.py
+++ src/mtcrawler/mtcrawler/parse_html_to_json.py
@@ -9,10 +9,19 @@ from scrapy.selector import Selector
 DATA_DIR = "/home/sujit/Projects/mlia-examples/data/mtcrawler"
 DISCLAIMER_TEXT = "anything else to real world is purely incidental"
 
+blob_pattern = re.compile(r"<b[.*?>]*>[A-Z0-9:() ]+</b>")
+
+def load_url_map():
+    url_map = dict()
+    fin = open(os.path.join(DATA_DIR, "urls.csv"), 'rb')
+    for line in fin:
+        key, value = line.strip().split("\t")
+        url_map[key] = value
+    fin.close()
+    return url_map
+    
 def normalize(s):
     return urllib.unquote_plus(s)
-
-blob_pattern = re.compile(r"<b[.*?>]*>[A-Z0-9:() ]+</b>")
 
 def get_candidate_blob(line, sel):
     line = line.strip()
@@ -39,7 +48,7 @@ def get_candidate_blob(line, sel):
         return line
     return None
     
-def unblobify(text):
+def to_plain_text(text):
     if text is None:
         return text
     # convert br tags to newline
@@ -53,11 +62,39 @@ def md5_hash(s):
     m.update(s)
     return m.hexdigest()
 
+def add_json(json_obj, json_fid):
+    print "Add JSON for: %s :: %s" % (json_obj["category"], json_obj["sample"])
+    json_fname = "%04d.json" % (json_fid)
+    fout = open(os.path.join(json_dir, json_fname), 'wb')        
+    json.dump(json_obj, fout)
+    fout.close()
+
+def merge_lists(list1, list2):
+    merged_set = set(list1).union(set(list2))
+    return list(merged_set)
+    
+def update_json(json_obj, json_fid):
+    print "Update JSON %s.json for: %s :: %s" % (json_fid, json_obj["category"], 
+                                                 json_obj["sample"])
+    json_fname = "%04d.json" % (json_fid)
+    fin = open(os.path.join(json_dir, json_fname), 'rb')
+    orig_json_obj = json.load(fin)
+    fin.close()
+    cats = merge_lists(orig_json_obj["category"], json_obj["category"])
+    orig_json_obj["category"] = cats
+    keywords = merge_lists(orig_json_obj["keywords"], json_obj["keywords"])
+    orig_json_obj["keywords"] = keywords
+    fout = open(os.path.join(json_dir, json_fname), 'wb')
+    json.dump(orig_json_obj, fout)
+    fout.close()
+
+
+url_map = load_url_map()
 rawhtml_dir = os.path.join(DATA_DIR, "raw_htmls")
 json_dir = os.path.join(DATA_DIR, "jsons")
 os.makedirs(json_dir)
 json_fid = 0
-doc_digests = set()
+doc_digests = dict()
 for root, dirnames, filenames in os.walk(rawhtml_dir):
     for filename in filenames:
         in_path = os.path.join(root, filename)
@@ -65,15 +102,18 @@ for root, dirnames, filenames in os.walk(rawhtml_dir):
         text = fin.read()        
         fin.close()
         json_obj = {}
+        # extract URL from map
+        json_obj["url"] = url_map[md5_hash(text)]
         # extract metadata from directory structure
         json_obj["sample"] = normalize(in_path.split("/")[-1].replace(".html", ""))
-        json_obj["category"] = normalize(in_path.split("/")[-2])
+        json_obj["category"] = [normalize(in_path.split("/")[-2])]
         # extract metadata from specific tags in HTML
         sel = Selector(text=text, type="html")
         json_obj["title"] = sel.xpath("//title/text()").extract()[0]
         json_obj["keywords"] = [x.strip() for x in 
           sel.xpath('//meta[contains(@name, "keywords")]/@content').
-          extract()[0].split(",")]
+          extract()[0].split(",") 
+          if len(x.strip()) > 0]
         json_obj["description"] = sel.xpath(
           '//meta[contains(@name, "description")]/@content').extract()[0]
         # extract dynamic text blob from text using regex
@@ -84,12 +124,13 @@ for root, dirnames, filenames in os.walk(rawhtml_dir):
         if text_blob is None:
             print "=====", in_path
             continue
-        json_obj["text"] = unblobify(text_blob)
-        doc_digests.add(md5_hash(json_obj["text"]))
-        print "Output JSON for: %s :: %s" % (json_obj["category"], json_obj["sample"])
-        json_fname = "%04d.json" % (json_fid)
-        fout = open(os.path.join(json_dir, json_fname), 'wb')        
-        json.dump(json_obj, fout)
-        fout.close()
+        plain_text = to_plain_text(text_blob)
+        plaintext_hash = md5_hash(plain_text)
+        json_obj["text"] = plain_text
+        if doc_digests.has_key(plaintext_hash):
+            update_json(json_obj, doc_digests[plaintext_hash])
+        else:
+            add_json(json_obj, json_fid)
+            doc_digests[plaintext_hash] = json_fid            
         json_fid += 1
 print "# unique docs:", len(doc_digests)
